1c5b3b07c6c9fea1794ce594854aed4e0f6d6778
Breno Silva <bbreno100@live.com>
2018-08-09 10:48:02 -0300

    Update TCPclient.py

--- TCPclient.py
+++ TCPclient.py
@@ -1,7 +1,8 @@
 import socket
+msg = raw_input('message: ')
 ClientSock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ClientSock.connect(('localhost', 7007))
-ClientSock.send('oi')
+ClientSock.connect(('localhost', 6710))
+ClientSock.send(msg)
 ServerMessage = ClientSock.recv(1000)
 print ServerMessage
 ClientSock.close()
