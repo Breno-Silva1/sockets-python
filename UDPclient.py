4616ff438b1c26ed8ec590e5e8671002a5b5b0a5
Breno Silva <bbreno100@live.com>
2018-08-09 11:02:22 -0300

    Update UDPclient.py

--- UDPclient.py
+++ UDPclient.py
@@ -1,6 +1,7 @@
 import socket
+msg = raw_input('message: ')
 ClientSock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-ClientSock.sendto('oi', ('localhost', 7777))
+ClientSock.sendto(msg, ('localhost', 6720))
 (ServerMsg, (ServerIP, ServerPort)) = ClientSock.recvfrom(1000)
 print ServerMsg
 ClientSock.close()
